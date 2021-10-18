22b4ec82ee2fb8979c6283227636a2343c609fe7
rooyca <rooyca@localhost.localdomain>
2021-10-18 18:10:48 -0400

    🆕 Switching to FastAPI

--- app.py
+++ app.py
@@ -1,102 +1,68 @@
-import io
 import os
-import yaml
+import redis
 import random
-from pymongo import MongoClient
-from werkzeug.utils import secure_filename
-from flask import Flask, request, jsonify, send_file, after_this_request
-from azure.storage.blob import BlobServiceClient, ContainerClient
- 
-app = Flask(__name__)
+from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse
+from azure.storage.blob import ContainerClient
 
-#Reading the yaml file
-def load_config():
-    dir_root = os.path.dirname(os.path.abspath(__file__))
-    with open(dir_root + "/config.yaml", "r") as yamlfile:
-        return yaml.load(yamlfile, Loader=yaml.FullLoader)
+load_dotenv()
+app = FastAPI()
 
-load = load_config()
-conn_string = load['azure_storage_connectionstring']
-container = load['uploads_container_name']
-container_client = ContainerClient.from_connection_string(conn_string, container)
-mongo_client = load['mongodb_client']
+CONN_STRING = os.getenv('azure_storage_connectionstring')
+CONTAINER = os.getenv('uploads_container_name')
+HOST = os.getenv('redis_host')
+PORT = os.getenv('redis_port')
+PASSWORD = os.getenv('redis_password')
+CONTAINER_CLIENT = ContainerClient.from_connection_string(CONN_STRING, CONTAINER)
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'rar', 'zip', 'gzip', 'mp3', 'mp4', '3gp'])
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #16 MEGABYTES
-db = MongoClient(mongo_client).thingsTD.file_id
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'rar', 'zip', 'mp3', 'mp4', '3gp', 'avi', 'wav'])
+conn = redis.Redis(host=HOST, port=PORT, password=PASSWORD)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-class AzureBlobFileDownloader:
-  def __init__(self):
-    # Initialize the connection to Azure storage account
-    self.blob_service_client =  BlobServiceClient.from_connection_string(conn_string)
-    self.my_container = self.blob_service_client.get_container_client(container)
- 
+class BlobAzureDownloader:
   def save_blob_locally(self,blob):
-    bytes = self.my_container.get_blob_client(blob).download_blob().readall()
+    bytes = CONTAINER_CLIENT.get_blob_client(blob).download_blob().readall()
  
     with open(blob, "wb") as file:
       file.write(bytes)
-    return blob
+    return blob 
+ 
+@app.post('/upload')
+async def upload_file(file: UploadFile = File(...)):
 
-# ERROR HANDLING
-@app.errorhandler(404)
-def not_found_error(error):
-    return jsonify({'Error' : 'Are you sure about that?'})
+    if not allowed_file(file.filename):
+        raise HTTPException(status_code=400, detail="File type is not allowed")
 
-@app.errorhandler(413)
-def not_found_error(error):
-    return jsonify({'Error' : 'Booty so big, lawd! Have mercy'})
+    blob_client = CONTAINER_CLIENT.get_blob_client(file.filename)
+    blob_client.upload_blob(file.file)
+    id_file = random.randint(1000,9999)
+    conn.set(id_file, file.filename)
+    return {"success":True,
+            "details":{
+            "fileName":file.filename,
+            "fileType":file.content_type,
+            "ID":id_file
+            }
+        }
 
-# TWO ENDPOINTS (UPLOAD AND DOWNLOAD/ID)  
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    
-    if 'total_files' not in request.files:
-        return jsonify({'message' : 'No file part in the request'}),400
-    
-    for file in request.files.getlist('total_files'):      
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            upload(file, conn_string, container)
-            idf = random.randint(1000,10000)
-            status = db.file_id.insert_one({
-                "_id" : idf,
-                "name" : filename
-            })
-            return jsonify({'ID' : f'{idf}'}),201
-        else:
-            return jsonify({(file.filename) : 'File type is not allowed'}),415
+@app.get('/download/{id_file}', response_class=FileResponse)
+async def download_file(id_file):
+    idf = conn.get(id_file)
 
-def upload(file, conn_string, container):
-    blob_client = container_client.get_blob_client(secure_filename(file.filename))
-    blob_client.upload_blob(file)
+    if not idf:
+        raise HTTPException(status_code=404, detail="File not found")
 
-@app.route('/download/<idf>')
-def download_file(idf):
-    idf_search = db.file_id.find_one({"_id":int(idf)})
-
-    @after_this_request
-    def removing_file(response):
-        try:
-            db.file_id.delete_one({"_id":int(idf)})
-            container_client.delete_blob(blob=blob_name)
-            file_path = os.path.dirname(os.path.abspath(__file__))
-            os.remove(file_path+"/"+blob_name)
-        except Exception as error:
-            app.logger.error("Error removing or closing downloaded file handle", error)
-        return response
-
-    if idf_search:
-        blob_name = idf_search['name']
-        azure_blob_file_downloader = AzureBlobFileDownloader()
+    try:
+        blob_name = str(idf).split("'")[1]
+        azure_blob_file_downloader = BlobAzureDownloader()
         file_downloaded = azure_blob_file_downloader.save_blob_locally(blob_name)
-        return send_file(file_downloaded)
+        conn.delete(id_file)
+        CONTAINER_CLIENT.delete_blob(blob=blob_name)
+        return blob_name
 
-    else:
-        return jsonify({'Error':'ID file not Found'}),404
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    except:
+        return {"success":False,
+                "details":"Something went wrong"}
